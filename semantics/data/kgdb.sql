b2beb6b31ebc68761eb8e49b075fd8ed9db502cc
NayanikaEL <nayanika.kalita@elucidata.io>
2023-09-25 16:38:15 +0530

    adding updated kgdb.sql file using git lfs

--- semantics/data/kgdb.sql
+++ semantics/data/kgdb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20f54a860321d04216b293de4e81e71e4d63e7ced379ade91b215508780d573e
-size 182919754
+oid sha256:1c9fbd3caffe577062c2576d90af2ccd8cea15442f7ffbf5b3874941020e2909
+size 185583792
